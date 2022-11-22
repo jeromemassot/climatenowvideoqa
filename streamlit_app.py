cfbe714fa56983d167a7b754637723c6517b4ee7
Jerome Massot <jerome.massot.78@gmail.com>
2022-11-21 22:34:05 -0800

    add better answering protocol and fix filters bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,67 +5,87 @@ from sentence_transformers import SentenceTransformer
 from nltk.tokenize import sent_tokenize
 from transformers import pipeline
 
+from pinecone import PineconeProtocolError
+import pinecone
+
 import streamlit as st
 import pandas as pd
-import pinecone
-import nltk
-
-# download nltk component
-nltk.download('punkt')
 
 
 @st.experimental_singleton
 def init_pinecone():
-    api_key = st.secrets['API_KEY']
+    """
+    Init the pinecone index with api key which is treated as secret
+    when the app is deployed on Streamlit Cloud.
+    :return: pinecone index object
+    """
+    api_key = "701ae398-351c-4a49-9a3d-586501bbd09b" #st.secrets['API_KEY']
     pinecone.init(api_key=api_key, environment='us-west1-gcp')
     return pinecone.Index('video-search')
     
 
 @st.experimental_singleton
 def init_retriever():
+    """
+    Return the LLM indicated in the code
+    :return: Language Model object
+    """
     return SentenceTransformer('multi-qa-mpnet-base-dot-v1')
 
 
 @st.experimental_singleton
 def load_qa_pipeline():
-    return pipeline("question-answering")
+    """
+    Load the HuggingFace pipeline for Question-Answering
+    :return: HuggingFace pipeline object
+    """
+    return pipeline("question-answering", model='deepset/roberta-base-squad2')
 
 
-topics = [
-    'biomass', 'climate', 'concrete', 'cost', 'efficiency', 'electricity',
-    'emission', 'energy', 'finance', 'fuel', 'hydrogen', 'industry',
-    'mineral', 'model', 'nuclear', 'ocean', 'plastic', 'policy',
-    'regulation', 'risk', 'scope', 'shipping', 'soil', 'solar',
-    'steel', 'storage', 'technology', 'trading', 'transportation',
-    'tree', 'vegetation', 'war', 'warming', 'waste', 'water', 'wind'
-]
-
-
-def reconstruct_answered_context(query, top_k=3, nature=['podcast', 'video'], topics=topics):
+def reconstruct_answered_context(index, query, top_k=3, nature=['Video', 'Podcast']):
+    """
+    Find the contexts used for the Q&A engine using the query
+    :param query: question asked by the user
+    :param top_k: number of contexts to return from the index
+    :param only_context: do not generate answer if True
+    :return: list of sentences as string objects
+    """
  
     # embed the query
     xq = retriever.encode([query]).tolist()
     
     # search the contexts
-    xc = index.query(
-        xq, 
-        top_k=top_k, 
-        filter={"keywords": {"$in": topics}, "nature": {"$in": nature}},
-        include_metadata=True
-    )
+    try:
+        xc = index.query(
+            xq, 
+            top_k=top_k,
+            filter={'nature': {"$in": nature}},
+            include_metadata=True
+        )
+    except PineconeProtocolError:
+        index = init_pinecone()
+        xc = index.query(
+            xq, 
+            top_k=top_k,
+            filter={'nature': {"$in": nature}},
+            include_metadata=True
+        )
     
-    # reconstruct the contexts
-    returned_sentences = set()
+    # reconstruct the contexts and find the answers
+    returned_contexts_answers = list()
     for context in xc['matches']:
         text_context = context['metadata']['text']
-        sentences = sent_tokenize(text_context)
-        answer = question_answerer(question=query, context=text_context)['answer']
-        for sentence in sentences:
-            if sentence.find(answer) > -1:
-                returned_sentences.add((sentence, context['metadata']['url'], context['metadata']['index']))
-                break
 
-    return returned_sentences
+        returned_contexts_answers.append({
+            'title': context['metadata']['title'],
+            'text': text_context,
+            'nature': context['metadata']['nature'],
+            'url': context['metadata']['url'], 
+            'keywords': context['metadata']['keywords'],
+            'start': context['metadata']['start_second']
+        })
+                
+    return returned_contexts_answers
 
 # interface
 
@@ -74,33 +94,63 @@ st.image("./decorations/logo-climate-now.svg", width=100)
 st.title("ClimateNow Videos and Podcasts Q&A Engine")
 st.subheader("Explore knowledge contained in ClimateNow Video and Podcast channel")
 
-st.info(
-    """
-    Disclaimer: the content shown in this page is the property of Climate Now. 
-    Petroglyphs NLP Consulting has only done the content parsing, the indexing and the 
-    Semantic Search Engine.
-    """
-)
-
 # Index and Retriever model setup
 index = init_pinecone()
 retriever = init_retriever()
 question_answerer = load_qa_pipeline()
 
 query = st.text_input("Question:", help="enter your question here")
-
-filter_nature = st.multiselect(label="Media Type", options=['podcast', 'video'])
-filter_topics = st.multiselect(label="Topics", options=topics)
-
-top_k = st.number_input("Nb of returned context:", 1, 5, help="Ranking contexts from videos")
-search = st.button("Search")
+filter_nature = st.multiselect(label="Media Type", options=['Podcast', 'Video'], help="Search Podcasts and/or Videos")
+top_k = st.number_input("Nb of returned context:", 1, 3, help="Top 3 ranking contexts maximum")
+search = st.button("Show me possible contexts and Answer...")
 
 if search and query != "":
-    returned_sentences = list(reconstruct_answered_context(query, top_k, filter_nature, filter_topics))
-    columns = st.columns(len(returned_sentences))
-    for i, col in enumerate(columns):
-        with col:
-            start = int(returned_sentences[i][1].split('t=')[-1][:-1])
-            st.video(returned_sentences[i][1], start_time=start)
-            st.markdown(f"**Answer**: {returned_sentences[i][0]}")
-            st.markdown(f"**Topic**: {returned_sentences[i][2]}")
+    returned_contexts_answers = list(reconstruct_answered_context(index, query, top_k, filter_nature))
+
+    if returned_contexts_answers and len(returned_contexts_answers)>0:
+        merged_text = ''
+        columns = st.columns(len(returned_contexts_answers))
+        for i, col in enumerate(columns):
+            with col:
+                title = returned_contexts_answers[i]['title']
+                text = returned_contexts_answers[i]['text']
+                topics = returned_contexts_answers[i]['keywords']
+                url = returned_contexts_answers[i]['url']
+                nature = returned_contexts_answers[i]['nature']
+                start = int(returned_contexts_answers[i]['start'])
+
+                st.markdown(f'<div style="text-align: center;"><b>{title}</b></div>', unsafe_allow_html=True)
+                displayed_text = '... ' + text + '...'
+                st.caption(f'<div style="text-align: justify;">{displayed_text}</div>', unsafe_allow_html=True)
+                st.caption(' ')
+                st.video(url, start_time=start)
+                st.markdown(f"**Topic**: {', '.join(topics)}")
+                st.markdown(f"**Media**: {nature}")
+
+                merged_text += ' ' + text
+
+        answer = question_answerer(question=query, context=merged_text)['answer']
+        st.info(f'A possible answer to your question is: **{answer}**')
+
+    else:
+        st.warning("Nothing found, sorry...")
+
+
+st.warning(
+    """
+    Disclaimer: the content shown in this page is the exclusive property of Climate Now. 
+    """
+)
+
+bottom_column_1, bottom_column_2 = st.columns([1, 9])
+with bottom_column_1:
+    st.image("./decorations/logo-petroglyphs.jpg", width=60)
+
+with bottom_column_2:
+    st.caption(
+        """
+        If you are interested by adding similar Semantic Search Engine 
+        to your content, please contact Petroglyphs NLP Consulting 
+        (jerome.massot.78@gmail.com)
+        """
+    )
